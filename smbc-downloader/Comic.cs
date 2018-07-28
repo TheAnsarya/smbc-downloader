efff9fae5a24761fd68d2da209d28aac560093e8
Andy Hubbard <me@ansarya.com>
2018-07-27 23:48:57 -0400

    Quick second. Explained a lot more. todo: move to comic class

---
+++ smbc-downloader/Comic.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smbc_downloader {
+	class Comic {
+		// rough versioning of this class structure
+		public string schema_version = "0.1.1";
+
+		// fetch info date, when comic page was scraped
+		public DateTime fetch_date;
+
+		// If there is an error during construction and logging in or json constuction is available
+		public string error_message;
+
+		// Which comic is this, the first comic is ID == 1, 
+		public int ID;
+
+		// URL of this comic
+		public string URL;
+
+		// Date comic was published, pulled from the topmost blog post on the page
+		public DateTime PublishedDate;
+
+		// Used for days with multiple comics, 1 == first, 2 == second, etc.
+		// They are in posted order
+		// So 1 if the only comic of the day. If you have two bonus comics then:
+		// 1 == bonus comic #1, 2 == bonus comic #2, 3 == main comic
+		public int ForThisDate;
+
+		// The unique bit of the url at the end, also works as a title sort of
+		public string ShortURL;
+
+		// Filename of the comic
+		public string ComicFilename;
+
+		// URL of the comic image
+		public string ComicURL;
+
+		// Filename of the votey (the red button at the end of the comic)
+		public string VoteyFilename;
+
+		// URL of the votey image
+		public string VoteyURL;
+
+		// The hover-text/title-text
+		public string TitleText;
+
+		// URL clicking comic goes to (used for Soonish and stuff)
+		public string ClickURL;
+
+		// URL of the next comic
+		public string NextURL;
+
+		// Url to the "Buy a print" website/shop
+		public string BuyAPrintURL;
+
+		public Comic(
+				DateTime fetch_date,
+				string error_message,
+				int ID,
+				string URL,
+				DateTime PublishedDate,
+				int ForThisDate,
+				string ShortURL,
+				string ComicFilename,
+				string ComicURL,
+				string VoteyFilename,
+				string VoteyURL,
+				string TitleText,
+				string ClickURL,
+				string NextURL,
+				string BuyAPrintURL) {
+			this.fetch_date = fetch_date;
+			this.error_message = error_message;
+			this.ID = ID;
+			this.URL = URL;
+			this.PublishedDate = PublishedDate;
+			this.ForThisDate = ForThisDate;
+			this.ShortURL = ShortURL;
+			this.ComicFilename = ComicFilename;
+			this.ComicURL = ComicURL;
+			this.VoteyFilename = VoteyFilename;
+			this.VoteyURL = VoteyURL;
+			this.TitleText = TitleText;
+			this.ClickURL = ClickURL;
+			this.NextURL = NextURL;
+			this.BuyAPrintURL = BuyAPrintURL;
+		}
+
+		public static string AddFromPage(int id, string link, string page) {
+
+			return null;
+		}
+	}
+}
--- smbc-downloader/Program.cs
+++ smbc-downloader/Program.cs
@@ -8,28 +8,82 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+// Note: this is probably a terrible example of how to program, but it gets the job done
 namespace smbc_downloader {
 	class Program {
+		
+		/**************************************************************
+		/* File names
+		/**************************************************************/
+
+		
+		// Everything on the console screen is also piped here
 		static string LogFileName = @"c:\working\smbc\{now}\log.txt";
+
+		// Errors get logged here as well so if 0 bytes then no errors
+		// TODO: Doesn't look like we're using this yet. Should really add some error handling
 		static string ErrorFileName = @"c:\working\smbc\{now}\error.txt";
-		static string StartURL = @"https://www.smbc-comics.com/comic/2002-09-05"; // @"https://www.smbc-comics.com/comic/literary-analysis";
+
+		// First page downloaded everything parses and progresses from here
+		// As a default, is the downloaded version of https://www.smbc-comics.com/comic/2002-09-05
+		// TODO: Probably gonna remove this and process StartURL to treat it like every other page
 		static string StartFileName = @"c:\working\smbc\{now}\start.txt";
-		static string PageName = @"c:\working\smbc\{now}\pages\{i}_{date}_{title}.txt";
+
+		// TODO: Gonna have to go through these and make sure they suit our purposes
+		static string PageName = @"c:\working\smbc\{now}\pages\{i}_{date}_{forthisdate}_{title}.txt";
 		static string ComicName = @"c:\working\smbc\{now}\comics\{name}";
 		static string Votey = @"c:\working\smbc\{now}\votey\{name}";
 		static string ByDateComicName = @"c:\working\smbc\{now}\comics-bydate\{date}\{name}";
 		static string ByDateVotey = @"c:\working\smbc\{now}\comics-bydate\votey\{name}";
 
 
+		/**************************************************************
+		/* URLs
+		/**************************************************************/
+
+		
+		// First page downloaded everything parses and progresses from here
+		//static string StartURL = @"https://www.smbc-comics.com/comic/2002-09-05";
+		static string StartURL = @"https://www.smbc-comics.com/comic/bar-joke-2";
+
+
+		/**************************************************************
+		/* Other
+		/**************************************************************/
+
+
+		// ID, starts at 0, which comic is this
+		// 0 means nothing has been downloaded, ID++; first comic has ID == 1
+		static int ID = 0;
+
+		// Program start time in yyyy-MM-dd_HH-mm-ss-ffff format
+		static string NowString = Now();
+
+		// Sometimes you need to fake the date
 		static string DiagnosticNow = @"2018-07-26_21-40-28-4545";
 
-		static string NowString = Now();
+		// Null
+		static DateTime VoidDate = new DateTime(1840, 1, 1);
+
+		/**************************************************************
+		/* Regular Expressions
+		 * The heart of this whole thing
+		/**************************************************************/
+
 
 		// Will be two, should be the same, check and error if not but continue
 		// Group 1: url of next comic page
 		static Regex nextUrlRegex = new Regex(@"class=""cc-next"" rel=""next"" href=""(.*?)"">");
 
-		// Group 1: url clicking comic goes to (use for Soonish and stuff)
+		// Used to grab the short redirect url, also acts as kind of a title
+		// Group 1: unique short redirect url piece
+		static Regex shorUrlRegex = new Regex(@"https:\/\/www.smbc-comics.com\/comic\/(.+)");
+
+		// The actual title
+		// Group 1: comic title
+		static Regex titleRegex = new Regex(@"<title>Saturday Morning Breakfast Cereal - (.*?)</title>");
+
+		// Group 1: url clicking comic goes to (used for Soonish and stuff)
 		// Group 2: title text
 		// Group 3: url of comic image
 		static Regex comicRegex = new Regex(@"<div id=""cc-comicbody""><a href=""(.*?)""><img title=""(.*?)"" src=""(.*?)"" id=""cc-comic"" \/>");
@@ -43,21 +97,44 @@ namespace smbc_downloader {
 		// This is actually the published time of the top blog post, so it should be the date of comic but you know
 		// Use the first one as that's the most recent comment at time of posting the comment.
 		// Ignore everything > 1(first) for now, incorporate the blog posts later
+		static Regex publishedDateRegex = new Regex(@"<div class=""cc-publishtime"">Posted (.*?) at (.*?)<\/div>");
+
+		/*
 		// Group 1: published month, in longform, ex "September"
 		// Group 2: published day of month, ex "5"
 		// Group 3: published year, ex. "2002"
 		// Group 4: published time, ex. "12:00"
 		// Group 5: published am/pm: ex. "am"
 		static Regex publishedDateRegex = new Regex(@"<div class=""cc-publishtime"">Posted ([A-Za-z]+) (\d+), (\d{4}) at (\d+:\d\d) (am|pm)<\/div>");
+		*/
 
+		static Regex TodaysNewsTitle = new Regex(@"<div class=""cc-newsarea""><div class=""cc-newsheader"">(.*?)</div>");
+
+		static Regex TodaysNewsContent = new Regex(@"<div class=""cc-newsbody""><p>(.*?)</p><div style=""padding:10px;clear:both;"">");
+
+
+
+		// Characters not allowed in windows file names so you can replace them with another character
+		// We should also check for CON, PRN, etc... so this of limitied usefulness, not fully robust
+		// Don't think I need it for this project but it's available if I do
 		static Regex filenameRegex = new Regex(@"[\*\.""\/\?\\:;|=,]");
 
+
+		/**************************************************************
+		/* Regular Expressions
+		 * The heart of this whole thing
+		/**************************************************************/
+
+		
+		// Entry point
+		// Can be used to run test methods instead of the full program if needed
 		static void Main(string[] args) {
-			DownloadYTS();
+			DownloadSMBC();
 			//DownloadTypes();
 		}
 
-		static void DownloadYTS() {
+		// Here we go, the actual program
+		static void DownloadSMBC() {
 			FixFileNames();
 			Directories();
 
@@ -68,15 +145,31 @@ namespace smbc_downloader {
 			string startName = String.Format(StartFileName, Now());
 			Log("start file - " + startName);
 			web.DownloadFile(StartURL, startName);
+
+			string text = File.ReadAllText(startName);
+			MatchCollection startMatches = publishedDateRegex.Matches(text);
+			Log("start match count - " + startMatches.Count);
+
+			DateTime publishedDate = VoidDate;
+			if (startMatches.Count == 0) {
+				Error("No published date");
+			} else {
+				publishedDate = DateTime.Parse(startMatches[0].Groups[1].Value + " " + startMatches[0].Groups[2].Value);
+				Log("published date - " + publishedDate.ToString("yyyy-MM-dd_HH-mm-ss-ffff"));
+			}
+
+
+
+
+
 			Wait();
 
-
-
-
+			web.Dispose();
 
 			End();
 		}
 
+		// Alter/preface?? (find right word) all the file paths with the DateTime the program was started
 		static void FixFileNames() {
 			LogFileName = LogFileName.Replace("{now}", NowString);
 			ErrorFileName = ErrorFileName.Replace("{now}", NowString);
@@ -89,6 +182,7 @@ namespace smbc_downloader {
 			ByDateVotey = ByDateVotey.Replace("{now}", NowString);
 		}
 
+		// Create directories for the files
 		static void Directories() {
 			mkdir(LogFileName);
 			mkdir(ErrorFileName);
@@ -97,9 +191,10 @@ namespace smbc_downloader {
 			mkdir(ErrorFileName);
 			mkdir(ComicName);
 			mkdir(Votey);
-			mkdir(ByDateVotey); 
+			mkdir(ByDateVotey);
 		}
 
+		// Utility method to make a directory
 		static void mkdir(string path) {
 			string dirName = Path.GetDirectoryName(path);
 			if (!Directory.Exists(dirName)) {
@@ -107,6 +202,7 @@ namespace smbc_downloader {
 			}
 		}
 
+		// Log file stream that records eveything that appears on the console
 		static StreamWriter _logFile;
 		static StreamWriter LogFile {
 			get {
@@ -117,6 +213,8 @@ namespace smbc_downloader {
 			}
 		}
 
+		// Log file stream that records all the errors encountered
+		// TODO: Not using this in this project yet. Add error handling
 		static StreamWriter _errorFile;
 		static StreamWriter ErrorFile {
 			get {
